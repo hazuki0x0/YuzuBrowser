0f8cf4104fc6880ea719c28e7e1b794e78c65c2d
hazuki <hazuki0x0@users.noreply.github.com>
2017-03-12 08:29:54 +0900

    change speed dial ui

--- app/src/main/java/jp/hazuki/yuzubrowser/speeddial/view/SpeedDialEditCallBack.java
+++ app/src/main/java/jp/hazuki/yuzubrowser/speeddial/view/SpeedDialEditCallBack.java
@@ -2,10 +2,6 @@ package jp.hazuki.yuzubrowser.speeddial.view;
 
 import jp.hazuki.yuzubrowser.speeddial.SpeedDial;
 
-/**
- * Created by hazuki on 17/02/20.
- */
-
 interface SpeedDialEditCallBack {
     void onEdited(SpeedDial speedDial);
 }
