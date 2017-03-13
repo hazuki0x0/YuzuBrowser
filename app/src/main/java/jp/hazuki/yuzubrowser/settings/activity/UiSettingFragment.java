1f83ad37faa4bc0c8d6e6e835077c9858f189494
hazuki <hazuki0x0@gmail.com>
2017-03-13 23:18:54 +0900

    Modify restoration process

--- app/src/main/java/jp/hazuki/yuzubrowser/settings/activity/UiSettingFragment.java
+++ app/src/main/java/jp/hazuki/yuzubrowser/settings/activity/UiSettingFragment.java
@@ -1,16 +1,12 @@
 package jp.hazuki.yuzubrowser.settings.activity;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
-import jp.hazuki.yuzubrowser.BrowserActivity;
 import jp.hazuki.yuzubrowser.R;
 import jp.hazuki.yuzubrowser.settings.data.AppData;
+import jp.hazuki.yuzubrowser.utils.AppUtils;
 
 /**
  * Created by hazuki on 17/01/16.
@@ -26,7 +22,7 @@ public class UiSettingFragment extends PreferenceFragment {
         findPreference("theme_setting").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object newValue) {
-                restart();
+                AppUtils.restartApp(getActivity());
                 return true;
             }
         });
@@ -34,20 +30,9 @@ public class UiSettingFragment extends PreferenceFragment {
         findPreference("restart").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                restart();
+                AppUtils.restartApp(getActivity());
                 return true;
             }
         });
-    }
-
-    private void restart() {
-        Intent intent = new Intent(getActivity(), BrowserActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(getActivity(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        AlarmManager manager = (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
-        Intent start = new Intent(getActivity(), BrowserActivity.class);
-        start.setAction(BrowserActivity.ACTION_FINISH);
-        start.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(start);
-        manager.setExact(AlarmManager.RTC, System.currentTimeMillis() + 800, pendingIntent);
     }
 }
