cb79322e5689da110a330d91981d7eadcd3ebe13
hazuki <hazuki0x0@gmail.com>
2017-03-21 22:53:29 +0900

    fixed download bug

---
+++ app/src/main/java/jp/hazuki/yuzubrowser/download/DownloadFileProvider.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2017 Hazuki
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package jp.hazuki.yuzubrowser.download;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.webkit.MimeTypeMap;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import jp.hazuki.yuzubrowser.BuildConfig;
+
+public class DownloadFileProvider extends ContentProvider {
+
+    public static final String PATH = "file";
+
+    private static final String SCHEME = "content";
+
+    private static final String AUTHORITY = BuildConfig.APPLICATION_ID + ".downloadFileProvider";
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public ParcelFileDescriptor openFile(@NonNull Uri uri, @NonNull String mode) throws FileNotFoundException {
+        return ParcelFileDescriptor.open(getFileForUri(uri), modeToMode(mode));
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        File file = getFileForUri(uri);
+        final int lastDot = file.getName().lastIndexOf('.');
+        if (lastDot >= 0) {
+            final String extension = file.getName().substring(lastDot + 1);
+            final String mime = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+            if (mime != null) {
+                return mime;
+            }
+        }
+
+        return "application/octet-stream";
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        File file = getFileForUri(uri);
+        return file.delete() ? 1 : 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    public static Uri getUriForFIle(File file) {
+        String path;
+        try {
+            path = file.getCanonicalPath();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Failed to resolve canonical path for " + file);
+        }
+        path = PATH + '/' + Uri.encode(path, "/");
+        return new Uri.Builder().scheme(SCHEME).authority(AUTHORITY).encodedPath(path).build();
+    }
+
+    public static File getFileForUri(Uri uri) {
+        String path = uri.getEncodedPath();
+
+        final int splitIndex = path.indexOf('/', 1);
+        final String tag = path.substring(1, splitIndex);
+        path = Uri.decode(path.substring(splitIndex + 1));
+
+        if (!PATH.equals(tag)) {
+            throw new IllegalArgumentException("No files supported by provider at " + uri);
+        }
+
+        File file = new File(path);
+
+        try {
+            file = file.getCanonicalFile();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Failed to resolve canonical path for " + file);
+        }
+        return file;
+    }
+
+    /**
+     * Copied from ContentResolver.java
+     */
+    private static int modeToMode(String mode) {
+        int modeBits;
+        if ("r".equals(mode)) {
+            modeBits = ParcelFileDescriptor.MODE_READ_ONLY;
+        } else if ("w".equals(mode) || "wt".equals(mode)) {
+            modeBits = ParcelFileDescriptor.MODE_WRITE_ONLY
+                    | ParcelFileDescriptor.MODE_CREATE
+                    | ParcelFileDescriptor.MODE_TRUNCATE;
+        } else if ("wa".equals(mode)) {
+            modeBits = ParcelFileDescriptor.MODE_WRITE_ONLY
+                    | ParcelFileDescriptor.MODE_CREATE
+                    | ParcelFileDescriptor.MODE_APPEND;
+        } else if ("rw".equals(mode)) {
+            modeBits = ParcelFileDescriptor.MODE_READ_WRITE
+                    | ParcelFileDescriptor.MODE_CREATE;
+        } else if ("rwt".equals(mode)) {
+            modeBits = ParcelFileDescriptor.MODE_READ_WRITE
+                    | ParcelFileDescriptor.MODE_CREATE
+                    | ParcelFileDescriptor.MODE_TRUNCATE;
+        } else {
+            throw new IllegalArgumentException("Invalid mode: " + mode);
+        }
+        return modeBits;
+    }
+}
