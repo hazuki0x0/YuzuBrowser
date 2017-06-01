52e3d95eb4b6715e1b7818029d9e225e8dd63d0f
hazuki <hazuki0x0@users.noreply.github.com>
2017-06-01 22:46:53 +0900

    fix speed dial icon's mime type

--- app/src/main/java/jp/hazuki/yuzubrowser/speeddial/WebIcon.java
+++ app/src/main/java/jp/hazuki/yuzubrowser/speeddial/WebIcon.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 Hazuki
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package jp.hazuki.yuzubrowser.speeddial;
 
 import android.graphics.Bitmap;
@@ -8,10 +24,6 @@ import android.util.Base64;
 import java.io.Serializable;
 
 import jp.hazuki.yuzubrowser.utils.ImageUtils;
-
-/**
- * Created by hazuki on 17/02/19.
- */
 
 public class WebIcon implements Serializable {
     private final byte[] iconBytes;
@@ -29,7 +41,7 @@ public class WebIcon implements Serializable {
     }
 
     public String getIconBase64() {
-        return "data:image/webp;base64," + new String(Base64.encode(iconBytes, Base64.DEFAULT));
+        return "data:image/png;base64," + new String(Base64.encode(iconBytes, Base64.DEFAULT));
     }
 
     public Bitmap getBitmap() {
