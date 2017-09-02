2f4a5ba74505d66dbc354cde2dc41de01b572b1b
hazuki <hazuki0x0@users.noreply.github.com>
2017-09-02 19:00:10 +0900

    new html bookmark decoder

--- app/src/main/java/jp/hazuki/yuzubrowser/bookmark/netscape/BookmarkHtmlImportTask.java
+++ app/src/main/java/jp/hazuki/yuzubrowser/bookmark/netscape/BookmarkHtmlImportTask.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 Hazuki
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package jp.hazuki.yuzubrowser.bookmark.netscape;
 
 import android.content.Context;
@@ -5,11 +21,7 @@ import android.os.Handler;
 import android.support.v4.content.AsyncTaskLoader;
 import android.widget.Toast;
 
-import org.xmlpull.v1.XmlPullParserException;
-
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 
 import jp.hazuki.yuzubrowser.R;
@@ -18,7 +30,6 @@ import jp.hazuki.yuzubrowser.bookmark.BookmarkManager;
 
 public class BookmarkHtmlImportTask extends AsyncTaskLoader<Boolean> {
 
-    private Context context;
     private File html;
     private BookmarkManager manager;
     private BookmarkFolder folder;
@@ -26,7 +37,6 @@ public class BookmarkHtmlImportTask extends AsyncTaskLoader<Boolean> {
 
     public BookmarkHtmlImportTask(Context context, File html, BookmarkManager manager, BookmarkFolder folder, Handler handler) {
         super(context);
-        this.context = context;
         this.html = html;
         this.manager = manager;
         this.folder = folder;
@@ -35,22 +45,14 @@ public class BookmarkHtmlImportTask extends AsyncTaskLoader<Boolean> {
 
     @Override
     public Boolean loadInBackground() {
-        try (FileReader fileReader = new FileReader(html);
-             BufferedReader reader = new BufferedReader(fileReader)) {
-
-            NetscapeBookmarkParser parser = new NetscapeBookmarkParser(folder);
-            parser.parse(reader);
+        try {
+            NetscapeBookmarkParser parser = new NetscapeBookmarkParser(getContext(), folder);
+            parser.parse(html);
             manager.write();
             return Boolean.TRUE;
-
         } catch (NetscapeBookmarkException e) {
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(context, R.string.not_bookmark_file, Toast.LENGTH_SHORT).show();
-                }
-            });
-        } catch (XmlPullParserException | IOException e) {
+            handler.post(() -> Toast.makeText(getContext(), R.string.not_bookmark_file, Toast.LENGTH_SHORT).show());
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return Boolean.FALSE;
