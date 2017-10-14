427f36c83dbbdd046c8544093969df03a1829fc8
hazuki <hazuki0x0@users.noreply.github.com>
2017-10-14 12:18:45 +0900

    convert to kotlin

---
+++ app/src/main/java/jp/hazuki/yuzubrowser/settings/activity/YuzuPreferenceFragment.kt
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2017 Hazuki
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jp.hazuki.yuzubrowser.settings.activity
+
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v7.preference.Preference
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompatDividers
+import jp.hazuki.yuzubrowser.R
+import jp.hazuki.yuzubrowser.settings.data.AppData
+import jp.hazuki.yuzubrowser.settings.preference.NightModePreference
+import jp.hazuki.yuzubrowser.settings.preference.SearchUrlPreference
+import jp.hazuki.yuzubrowser.settings.preference.SlowRenderingPreference
+import jp.hazuki.yuzubrowser.settings.preference.WebTextSizePreference
+import jp.hazuki.yuzubrowser.settings.preference.common.*
+
+abstract class YuzuPreferenceFragment : PreferenceFragmentCompatDividers() {
+
+    var preferenceResId: Int = 0
+        private set
+
+    abstract fun onCreateYuzuPreferences(savedInstanceState: Bundle?, rootKey: String?)
+
+    override fun onCreatePreferencesFix(savedInstanceState: Bundle?, rootKey: String?) {
+        preferenceManager.sharedPreferencesName = AppData.PREFERENCE_NAME
+        onCreateYuzuPreferences(savedInstanceState, rootKey)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        try {
+            return super.onCreateView(inflater, container, savedInstanceState)
+        } finally {
+            setDividerPreferences(PreferenceFragmentCompatDividers.DIVIDER_PADDING_CHILD or PreferenceFragmentCompatDividers.DIVIDER_CATEGORY_BETWEEN or PreferenceFragmentCompatDividers.DIVIDER_NO_BEFORE_FIRST)
+            preferenceManager.sharedPreferencesName = AppData.PREFERENCE_NAME
+            val a = activity.theme.obtainStyledAttributes(intArrayOf(android.R.attr.listDivider))
+            val divider = a.getDrawable(0)
+            a.recycle()
+            setDivider(divider)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen?.run {
+            val key: String? = arguments?.getString(ARG_PREFERENCE_ROOT)
+            val title = if (!TextUtils.isEmpty(key)) findPreference(key).title else title
+            activity.title = if (TextUtils.isEmpty(title)) getText(R.string.pref_settings) else title
+        }
+    }
+
+    override fun addPreferencesFromResource(preferencesResId: Int) {
+        super.addPreferencesFromResource(preferencesResId)
+        this.preferenceResId = preferencesResId
+    }
+
+    override fun setPreferencesFromResource(preferencesResId: Int, key: String?) {
+        super.setPreferencesFromResource(preferencesResId, key)
+        this.preferenceResId = preferencesResId
+    }
+
+    override fun onDisplayPreferenceDialog(preference: Preference) {
+        if (this.fragmentManager.findFragmentByTag(FRAGMENT_DIALOG_TAG) == null) {
+            val dialog: DialogFragment
+
+            when (preference) {
+                is StrToIntListPreference -> dialog = StrToIntListPreference.PreferenceDialog.newInstance(preference)
+                is AlertDialogPreference -> dialog = AlertDialogPreference.PreferenceDialog.newInstance(preference)
+                is FloatSeekbarPreference -> dialog = FloatSeekbarPreference.PreferenceDialog.newInstance(preference)
+                is IntListPreference -> dialog = IntListPreference.PreferenceDialog.newInstance(preference)
+                is MultiListIntPreference -> dialog = MultiListIntPreference.PrefernceDialog.newInstance(preference)
+                is SeekbarPreference -> dialog = SeekbarPreference.PreferenceDialog.newInstance(preference)
+                is SearchUrlPreference -> dialog = SearchUrlPreference.PreferenceDialog.newInstance(preference)
+                is NightModePreference -> dialog = NightModePreference.SettingDialog.newInstance(preference)
+                is WebTextSizePreference -> dialog = WebTextSizePreference.SizeDialog.newInstance(preference)
+                is SlowRenderingPreference -> dialog = SlowRenderingPreference.WarningDialog.newInstance(preference)
+                is CustomDialogPreference -> {
+                    preference.show(childFragmentManager)
+                    return
+                }
+                else -> {
+                    super.onDisplayPreferenceDialog(preference)
+                    return
+                }
+            }
+
+            dialog.setTargetFragment(this, 0)
+            dialog.show(fragmentManager, preference.key)
+        }
+    }
+
+    companion object {
+        private const val FRAGMENT_DIALOG_TAG = "android.support.v7.preference.PreferenceFragment.DIALOG"
+    }
+}
