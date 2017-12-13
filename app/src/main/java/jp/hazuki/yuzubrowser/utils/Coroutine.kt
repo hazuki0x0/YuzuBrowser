cff128510746e42d6644bb55e9f0a9b10d68fa4b
hazuki <hazuki0x0@users.noreply.github.com>
2017-12-13 20:30:31 +0900

    update kotlin coroutines

--- app/src/main/java/jp/hazuki/yuzubrowser/utils/Coroutine.kt
+++ app/src/main/java/jp/hazuki/yuzubrowser/utils/Coroutine.kt
@@ -16,18 +16,15 @@
 
 package jp.hazuki.yuzubrowser.utils
 
-import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.CoroutineScope
-import kotlinx.coroutines.experimental.CoroutineStart
+import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.launch
 import kotlin.coroutines.experimental.CoroutineContext
 import kotlinx.coroutines.experimental.async as ktAsync
 
 typealias Deferred<T> = kotlinx.coroutines.experimental.Deferred<T>
 
-fun <T> async(context: CoroutineContext = CommonPool, start: CoroutineStart = CoroutineStart.DEFAULT, block: suspend CoroutineScope.() -> T)
-        = ktAsync(context, start, block)
+fun <T> async(context: CoroutineContext = CommonPool, start: CoroutineStart = CoroutineStart.DEFAULT, parent: Job? = null, block: suspend CoroutineScope.() -> T)
+        = ktAsync(context, start, parent, block)
 
-fun ui(start: CoroutineStart = CoroutineStart.DEFAULT, block: suspend CoroutineScope.() -> Unit)
-        = launch(UI, start, block)
+fun ui(start: CoroutineStart = CoroutineStart.DEFAULT, parent: Job? = null, block: suspend CoroutineScope.() -> Unit)
+        = launch(UI, start, parent, block)
