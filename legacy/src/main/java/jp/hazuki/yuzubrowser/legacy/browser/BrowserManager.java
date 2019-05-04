cc9c51b512f7917d6dec4f805e557de6c5d30a3e
hazuki <hazuki0x0@users.noreply.github.com>
2019-05-04 16:45:51 +0900

    remove unused code

--- legacy/src/main/java/jp/hazuki/yuzubrowser/legacy/browser/BrowserManager.java
+++ legacy/src/main/java/jp/hazuki/yuzubrowser/legacy/browser/BrowserManager.java
@@ -20,13 +20,10 @@ import android.content.Context;
 import android.webkit.GeolocationPermissions;
 import android.webkit.WebStorage;
 
-import java.io.File;
-
 import jp.hazuki.yuzubrowser.core.utility.utils.FileUtils;
 
 public class BrowserManager {
     public static final String EXTRA_OPENABLE = "jp.hazuki.yuzubrowser.BrowserActivity.extra.EXTRA_OPENABLE";
-    public static final String EXTRA_LOAD_URL_TAB = "jp.hazuki.yuzubrowser.BrowserActivity.extra.EXTRA_LOAD_URL_TAB";
     //same as pref_newtab_values
     public static final int LOAD_URL_TAB_CURRENT = 0;
     public static final int LOAD_URL_TAB_NEW = 1;
@@ -34,16 +31,6 @@ public class BrowserManager {
     public static final int LOAD_URL_TAB_NEW_RIGHT = 3;
     public static final int LOAD_URL_TAB_BG_RIGHT = 4;
     public static final int LOAD_URL_TAB_CURRENT_FORCE = 5;
-
-    @Deprecated
-    public static File getAppCacheFile(Context context) {
-        return context.getDir("appcache", Context.MODE_PRIVATE);
-    }
-
-    @Deprecated
-    public static String getAppCacheFilePath(Context context) {
-        return getAppCacheFile(context).getAbsolutePath();
-    }
 
     public static void clearAppCacheFile(Context context) {
         FileUtils.deleteFile(context.getDir("appcache", Context.MODE_PRIVATE));
