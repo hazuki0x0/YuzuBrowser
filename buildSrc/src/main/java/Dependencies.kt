63408d40ea7fb3ae408c09cc1a8ec3f935fb2249
hazuki <hazuki0x0@users.noreply.github.com>
2019-06-02 01:26:40 +0900

    Version 5.1.0 beta2

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -22,8 +22,8 @@ object Build {
 }
 
 object AppVersions {
-    const val version_name = "5.1.0-beta1"
-    const val version_code = 410010
+    const val version_name = "5.1.0-beta2"
+    const val version_code = 410011
 }
 
 object Versions {
