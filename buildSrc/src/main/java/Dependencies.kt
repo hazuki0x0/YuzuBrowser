41b69d91dd5c04b8e0f14d5ea8a2ca86ef833c85
hazuki <hazuki0x0@users.noreply.github.com>
2021-03-01 22:48:01 +0900

    Version 6.0.2

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -22,8 +22,8 @@ object Build {
 }
 
 object AppVersions {
-    const val version_name = "6.0.1"
-    const val version_code = 410020
+    const val version_name = "6.0.2"
+    const val version_code = 410021
 }
 
 object Versions {
